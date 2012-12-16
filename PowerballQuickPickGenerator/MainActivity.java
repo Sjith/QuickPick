11f0d80baf34b82911796e8b901864492814ad04
Jose F. Rios <jrios001@gmail.com>
2012-12-15 21:08:18 -0500

    MainActivity no longer generates the numbers. This was moved to the DisplayNumbers Class.

--- PowerballQuickPickGenerator/MainActivity.java
+++ PowerballQuickPickGenerator/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.powerballquickpickgenerator;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.text.Editable;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -13,33 +15,52 @@ import java.util.Random;
 public class MainActivity extends Activity {
 
 	private static final boolean False = false;
-	static final String EXTRA_NUMBERS = "com.example.PowerBallQuickPickGenerator.NUMBERS";
+	static final String EXTRA_TICKETS = "com.example.PowerBallQuickPickGenerator.TICKETS";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
 	}
  	
  	public void getNumbers(View view) {
  		Intent intent = new Intent(this, DisplayNumbers.class);
+ 		EditText editText = (EditText) findViewById(R.id.numTickets);
+ 		Bundle b = new Bundle();
+ 		List tickets = totalTickets(numberOfTickets);
+ 		b.putStringArray(EXTRA_TICKETS, tickets);
+ 		intent.putExtras(b);
+ 		int numberOfTickets = editText.getText().toString();
 		startActivity(intent);
  	}
 
+ 	private static List totalTickets(int num) {
+		int i = 1;
+		List tickets = new ArrayList();
+		
+		// Generate the Powerball tickets
+		while (i <= num) {
+			tickets.add(getListOfNumbers() + " " + "Powerball: " + power());
+			i++;
+		}
+
+		return tickets;
+	}
+
   	// Generates a random number
-	public static int getRandomNumber(int min, int max) {
+	private static int getRandomNumber(int min, int max) {
 		Random random = new Random();
 		int randomNumber = random.nextInt(max - min) + min;
 		return randomNumber;
 	}
 
 	// Generate the list of random numbers (five random numbers between 1 and 59 inclusive)
-	public static String getListOfNumbers() {
+	private static String getListOfNumbers() {
 		List<Integer> numbers = new ArrayList<Integer>();
-
+		
 		while (numbers.size() != 5) {
-			int randNum = getRandomNumber(1,60);
+			int randNum = getRandomNumber(1,59);
 
 			if (numbers.contains(randNum) == False) {
 				numbers.add(randNum);
@@ -52,12 +73,12 @@ public class MainActivity extends Activity {
 		return numbers.toString();
 	}
 
-	public static String power() {
+	private static String power() {
 		// Generate the Powerball number (random number between 1 and 35 inclusive
-		return "" + getRandomNumber(1,36);
+		return "" + getRandomNumber(1,35);
 	}
 
-	public static List<Integer> sort(List<Integer> list) {
+	private static List sort(List list) {
 		Collections.sort(list);
 		return list;
 	}
--- PowerballQuickPickGenerator/src/com/example/powerballquickpickgenerator/DisplayNumbers.java
+++ PowerballQuickPickGenerator/src/com/example/powerballquickpickgenerator/DisplayNumbers.java
@@ -2,23 +2,20 @@ package com.example.powerballquickpickgenerator;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
 import android.widget.TextView;
 
 public class DisplayNumbers extends Activity {
+	
+	public String numbers = "" + MainActivity.getListOfNumbers() + " " + MainActivity.power();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_display_numbers);
 
-		// Get the message from the intent
-		Intent intent = getIntent();
-		String numbers = intent.getStringExtra(MainActivity.EXTRA_NUMBERS);
-
 		// Create the TextView
 		TextView textView = new TextView(this);
-		textView.setTextSize(30);
+		textView.setTextSize(25);
 	    textView.setText(numbers);
 
 	    // Set the text view as the activity layout
